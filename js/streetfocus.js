7362c60c89d16adb9f0753593b0be7658a79ce68
mvl22 <martin@lucas-smith.co.uk>
2021-09-04 21:58:50 +0100

    Fix broken image display in ideas when no image in data

--- js/streetfocus.js
+++ js/streetfocus.js
@@ -801,7 +801,7 @@ var streetfocus = (function ($) {
 			$(element + ' h2.title').text (feature.properties.title);
 			$(element + ' p.link a').attr ('href', feature.properties.link);
 			$(element + ' div.description').html (feature.properties.description);		// Will be paragraph(s) of HTML
-			if (feature.properties.image !== 'null') {
+			if (feature.properties.image !== null && feature.properties.image !== 'null') {
 				$(element + ' p.image').show ();
 				$(element + ' p.image img').attr ('src', feature.properties.image);
 			} else {
